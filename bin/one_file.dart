017685c7d24796b05bd971a33efb37d200787438
Xavier Hainaux <xavier.hainaux@gaming1.com>
2015-09-09 14:50:06 +0200

    Feature

--- bin/one_file.dart
+++ bin/one_file.dart
@@ -2,7 +2,15 @@ main() =>
     throw "Ce code n'est pas formatté correctement donc normalement ça devrait changer";
 
 myFunction() {
-  if (true) {
+  if(true) {
+    print('almost good');
     print('pasbien');
+  }
+}
+
+myFeature() {
+  if (true){
+    print('ma super feature');
+
   }
 }
